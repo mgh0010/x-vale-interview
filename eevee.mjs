c5718a1b4671cff6fd7ff5eddf73a387ce31c1e4
Michael Harrison <mghharrison@gmail.com>
2020-02-19 16:14:07 -0600

    first commit

---
+++ eevee.mjs
@@ -0,0 +1,28 @@
+export const eeveeAbilities = {
+  "abilities": [
+    {
+      "ability": {
+        "name": "anticipation",
+        "url": "https://pokeapi.co/api/v2/ability/107/"
+      },
+      "is_hidden": true,
+      "slot": 3
+    },
+    {
+      "ability": {
+        "name": "adaptability",
+        "url": "https://pokeapi.co/api/v2/ability/91/"
+      },
+      "is_hidden": false,
+      "slot": 2
+    },
+    {
+      "ability": {
+        "name": "run-away",
+        "url": "https://pokeapi.co/api/v2/ability/50/"
+      },
+      "is_hidden": false,
+      "slot": 1
+    }
+  ]
+}
---
+++ test.mjs
@@ -0,0 +1,7 @@
+import { eeveeAbilities  } from './eevee.mjs' 
+
+logAbilities();
+
+function logAbilities() {
+  console.log(eeveeAbilities.abilities[0]);
+} 
